2df8c9f1239cf3491f9ae0364bbfe37f985b93fc
nicholas426 <67550979+nicholas426@users.noreply.github.com>
2021-02-10 23:35:33 -0300

    Update badlib.lua

--- badlib.lua
+++ badlib.lua
@@ -4,15 +4,14 @@ function Library:CreateMain()
 	
 	local FruityMenu = Instance.new("ScreenGui")
 	local ScrollFrame = Instance.new("ScrollingFrame")
-	local Credit = Instance.new("TextLabel")
 	
 	
 	function onKeyPress(inputObject, gameProcessedEvent)
 		if inputObject.KeyCode == Enum.KeyCode.RightShift then
-			if FruityMenu.Visible == false then
-				FruityMenu.Visible = true
+			if ScrollFrame.Visible == false then
+				ScrollFrame.Visible = true
 			else
-				FruityMenu.Visible = false
+				ScrollFrame.Visible = false
 			end
 		end
 	end
@@ -25,8 +24,8 @@ function Library:CreateMain()
 	FruityMenu.Name = "FruityMenu"
 	FruityMenu.Parent = game:GetService("CoreGui")
 	FruityMenu.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	FruityMenu.Active = true
-	FruityMenu.Draggable = true
+	ScrollFrame.Active = true
+	ScrollFrame.Draggable = true
 
 	ScrollFrame.Name = "ScrollFrame"
 	ScrollFrame.Parent = FruityMenu
@@ -36,22 +35,7 @@ function Library:CreateMain()
 	ScrollFrame.Position = UDim2.new(0.764993846, 0, 0.168085098, 0)
 	ScrollFrame.Size = UDim2.new(0, 141, 0, 285)
 	ScrollFrame.CanvasPosition = Vector2.new(0, 69.2631607)
-	
-
-	Credit.Name = "Credit"
-	Credit.Parent = FruityMenu
-	Credit.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
-	Credit.BorderColor3 = Color3.fromRGB(0, 0, 0)
-	Credit.Position = UDim2.new(0.764828622, 0, 0.774468064, 0)
-	Credit.Size = UDim2.new(0, 141, 0, 22)
-	Credit.Font = Enum.Font.SourceSans
-	Credit.Text = "Created By BadBoy#7422"
-	Credit.TextColor3 = Color3.fromRGB(0, 0, 0)
-	Credit.TextScaled = true
-	Credit.TextSize = 14.000
-	Credit.TextStrokeColor3 = Color3.fromRGB(247, 0, 255)
-	Credit.TextStrokeTransparency = 0.000
-	Credit.TextWrapped = true	
+		
 	
 	local GamerLibrary = {}
 	
